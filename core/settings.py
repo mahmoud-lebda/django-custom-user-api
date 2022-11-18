36ff993819f3b78005469876e4c0e383bad79fc1
Mahmoud Lebda <mahmoud.lebda@gmail.com>
2022-11-18 13:29:00 +0200

    add swagger

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'drf_spectacular',  # sqagger
+
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',  # add swagger
+}
+
+SPECTACULAR_SETTINGS = {
+    # Swagger settings
+
+    'TITLE': 'Custom User API',
+    'DESCRIPTION': 'All Api for the custom user api',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'COMPONENT_SPLIT_REQUEST': True,
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Django Custom User API
+
+this project is an example for build custom user model and build api for authentication and api related to user (profile, rest password, ......)
+
+
