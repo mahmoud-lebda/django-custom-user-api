ed48e844ca8cd66422e4f195731ad2dc715b3db8
Mahmoud Lebda <mahmoud.lebda@gmail.com>
2022-11-19 15:13:21 +0200

    add smtp credintial to env

--- core/settings.py
+++ core/settings.py
@@ -184,16 +184,8 @@ AUTH_USER_MODEL = 'user.User'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_HOST_USER = 'mahmoud.lebda@scopeims.com'
-EMAIL_HOST_PASSWORD = '2aryBDsVcSn7'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
-
-# EMAIL_HOST = 'smtp.zoho.com'
-# EMAIL_HOST_USER = 'user@example.com'
-# EMAIL_HOST_PASSWORD = 'password'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
